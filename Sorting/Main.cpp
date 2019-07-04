83acc101fd53d9aa8821116766a8b4776460c577
Zhekuson <zhekuson@gmail.com>
2019-07-04 15:15:17 +0300

    added operations count for some algs

--- Sorting/Main.cpp
+++ Sorting/Main.cpp
@@ -1,5 +1,6 @@
 #include "SortingAlgos.cpp"
 
+
 int main(){
 	
 }
--- Sorting/SortingAlgos.cpp
+++ Sorting/SortingAlgos.cpp
@@ -1,7 +1,8 @@
 
 
 using namespace std;
-//todo count operations
+
+//сам swap весит 3 операции
 void swap(unsigned int& t1, unsigned int& t2) {
 	unsigned int temp = t1;
 	t1 = t2;
@@ -10,93 +11,151 @@ void swap(unsigned int& t1, unsigned int& t2) {
 //сортировка обычным пузырьком
 void bubbleSortSimple(unsigned int* arr, unsigned int array_size, unsigned int* result, int& operations) {
 	operations = 0;
+	
+	operations += 3;//i=0 присвоение и i < array_size - 1 
 	for (size_t i = 0; i < array_size - 1; i++)
 	{
+	
+		operations += 4;//i < arrsize-1, i++
+		operations += 3;// j = 0 j < arrsize-i
 		for (size_t j = 0; j < array_size - i; j++)
 		{
+			operations += 4;// j++ и сравнение с arrsize
+			operations += 4; // j+1, aj, aj+1, > 
 			if (arr[j] > arr[j + 1]) {
+				operations += 6;
 				swap(arr[j], arr[j + 1]);
 			}
 		}
 	}
+
+	operations += 1;
 	result = arr;
 }
 void bubbleSortIverson1(unsigned int* arr, unsigned int array_size, unsigned int* result, int& operations) {
 	operations = 0;
 	bool swapped;
+	operations += 3;//i=0 присвоение и i < array_size - 1 
 	for (size_t i = 0; i < array_size - 1; i++)
 	{
+		operations += 4;//i < arrsize-1, i++
+		operations += 1;//присвоение swapped 
 		swapped = false;
+		operations += 3;// j = 0 j < arrsize-i
 		for (size_t j = 0; j < array_size - i; j++)
 		{
+			operations += 4;// j++ и сравнение с arrsize
+			operations += 4; // j+1, a[j], a[j+1], > 
 			if (arr[j] > arr[j + 1]) {
+				operations += 6;
 				swap(arr[j], arr[j + 1]);
+				operations += 1;
 				swapped = true;
 			}
 		}
+		operations += 1;
 		if (!swapped) {
 			break;
 		}
 	}
+
+	operations += 1;
 	result = arr;
 }
 void bubbleSortIverson12(unsigned int* arr, unsigned int array_size, unsigned int* result, int& operations) {
 	operations = 0;
 	bool swapped;
+
+	operations += 2;
 	unsigned int lastSwapped = array_size - 1;
 	unsigned int Bound = array_size - 1;
+
+	operations += 3;
 	for (size_t i = 0; i < array_size - 1; i++)
 	{
+		operations += 4;//цикл
+
+		operations += 3;
 		Bound = lastSwapped;
 		lastSwapped = 0;
 		swapped = false;
+
+		operations += 2;
 		for (size_t j = 0; j < Bound; j++)
 		{
+			operations += 3;//цикл
+			operations += 4;//взятие и сравнение
 			if (arr[j] > arr[j + 1]) {
+
+				operations += 6;
 				swap(arr[j], arr[j + 1]);
+
+				operations += 2;
 				swapped = true;
 				lastSwapped = j;
 			}
 		}
+		operations += 1;
+		//нет смысла проверять lastswapped, потому что они меняются в одном месте
 		if (!swapped) {
 			break;
 		}
 	}
+
+	operations += 1;
 	result = arr;
 
 }
 void simpleInsertionsSort(unsigned int* arr, unsigned int array_size, unsigned int* result, int& operations) {
 	operations = 0;
+
+	operations += 2;
 	for (size_t i = 1; i < array_size; i++)
 	{
+		operations += 3;
+
+		operations += 3;
 		for (size_t j = i - 1; j >= 0; j--)
 		{
+			operations += 3;
+
+			operations += 3;
 			if (arr[i] < arr[j]) {
+				operations += 3;
 				arr[j + 1] = arr[j];
 			}
 			else
 			{
+				operations += 3;
 				arr[j + 1] = arr[i];
 			}
 		}
 	}
+
+	operations += 1;
 	result = arr;
 }
 void binaryInsertionsSort(unsigned int* arr, unsigned int array_size, unsigned int* result, int& operations) {
 	operations = 0;
 	for (size_t i = 1; i < array_size; i++)
 	{
-		for (size_t j = i - 1; j >= 0; j--)
+		unsigned int left = 0, right = i - 1 , mid;
+		while (right != left)
 		{
-			if (arr[i] < arr[j]) {
-				arr[j + 1] = arr[j];
+			mid = (right + left) / 2;
+			if (arr[mid] > arr[i]) {
+				left = mid;
 			}
-			else//todo binary
+			else
 			{
-				arr[j + 1] = arr[i];
+				right = mid - 1;
 			}
+
 		}
+		
 	}
+
+	operations += 1;
 	result = arr;
 }
 unsigned int find_Max(unsigned int* arr, unsigned int& array_size) {
